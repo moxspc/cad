483b266ecc28e379d56cb7baeb529e0e51777b44
Jayesh Salvi <jayeshsalvi@gmail.com>
2016-10-19 21:30:31 -0400

    add curved edge test

--- tests/tess.js
+++ tests/tess.js
@@ -3,7 +3,8 @@ var assert = require('assert');
 var moxcad = require('../build/Debug/moxcad');
 
 describe('Tess', function() {
-  it('tessellate', function () {
+
+  it('tessellate 1', function () {
     var point1 = new moxcad.geom.Point(0,0,0);
     var point2 = new moxcad.geom.Point(1,0,0);
     var point3 = new moxcad.geom.Point(1,1,0);
@@ -26,4 +27,33 @@ describe('Tess', function() {
     assert(bufferMesh.faceBuffers.length, 6);
     assert(bufferMesh.edgeBuffers.length, 12);
   });
+
+  it('tessellate 2', function () {
+    var point1 = new moxcad.geom.Point(0,0,0);
+    var point2 = new moxcad.geom.Point(1,0,0);
+    var point3 = new moxcad.geom.Point(1,1,0);
+    var point3_4 = new moxcad.geom.Point(0.5,1.5,0);
+    var point4 = new moxcad.geom.Point(0,1,0);
+
+    var lineseg1 = new moxcad.geom.LineSegment(point1, point2);
+    var lineseg2 = new moxcad.geom.LineSegment(point2, point3);
+    var circarc3 = new moxcad.geom.CircularArc(point3, point3_4, point4);
+    var lineseg4 = new moxcad.geom.LineSegment(point4, point1);
+
+    var edge1 = lineseg1.makeEdge();
+    var edge2 = lineseg2.makeEdge();
+    var edge3 = circarc3.makeEdge();
+    var edge4 = lineseg4.makeEdge();
+
+    var wire = new moxcad.topo.Wire(edge1, edge2, edge3, edge4);
+    var face = wire.makeFace();
+    var dir = new moxcad.geom.Vector(0,0,1);
+    var solid = moxcad.ops.extrude(face, dir);
+
+    var bufferMesh = solid.tessellate();
+    assert(!!bufferMesh);
+    assert(bufferMesh.faceBuffers.length, 6);
+    assert(bufferMesh.edgeBuffers.length, 12);
+  });
+
 });
